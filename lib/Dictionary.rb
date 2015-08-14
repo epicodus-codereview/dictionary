5bc092947196c48a480e0971fd2e5d3bea97a440
Abdul Rahimi <afr114@gmail.com>
2015-08-14 09:54:44 -0700

    it saves the word

--- lib/Dictionary.rb
+++ lib/Dictionary.rb
@@ -9,7 +9,12 @@ class Dictionary
     @word_id = @@all_words.length.+(1)
   end
 
+  define_singleton_method(:all) do
+    @@all_words
+  end
 
-
+  def save
+    @@all_words.push(self)
+  end
 
 end# end class
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -10,5 +10,12 @@ describe(Dictionary) do
     end
   end
 
+  describe('#save') do
+  it('saves the word') do
+    test_word = Dictionary.new("apple")
+    test_word.save()
+    expect(Dictionary.all()).to(eq([test_word]))
+  end
+end
 
 end#ends the spec
